cf412268705d5c625826fe5f5421353ae17ac05a
Doria <dorialassalle@yahoo.com>
2021-06-30 13:34:51 -0400

    terminado

--- poorly_coded_store/models.py
+++ poorly_coded_store/models.py
@@ -6,6 +6,9 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __repr__(self):
+        return f"Producto: {self.description}"
+
 class Order(models.Model):
     quantity_ordered = models.IntegerField()
     total_price = models.DecimalField(decimal_places=2, max_digits=6)
--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
 
 def index(request):
@@ -7,10 +7,39 @@ def index(request):
     }
     return render(request, "store/index.html", context)
 
+
 def checkout(request):
-    quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
-    print("Charging credit card...")
-    Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    if "quant" not in request.session:
+        request.session["quant"]=0
+    
+    if "charge" not in request.session:
+        request.session["charge"]=0
+
+    quantity_from_form = int(request.POST["quantity"])          #q de productos de la compra actual
+    idSelectedProduct=request.POST["price"]                     #traigo el id del producto que esta comprando
+    selectedProduct=Product.objects.get(id=idSelectedProduct)   #recupero el obj producto para usar el precio   
+    price_from_form = float(selectedProduct.price)              #guardo el precio en la variable
+   
+    total_charge_order = quantity_from_form * price_from_form   #calc la cant por el precio
+    request.session['quant']+=quantity_from_form
+    request.session["charge"]+=total_charge_order
+
+    print(f"Charging credit card...{total_charge_order}")
+    Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge_order) # se crea la orden
+   
+    ttlQtty= request.session['quant']
+    ttlCharge=request.session["charge"]
+
+    print(f"total:{ttlCharge}")  
+    print(f"cant total:{ttlQtty}")
+    
+    return redirect(f"/checking_out/{ttlQtty}/{total_charge_order}/{ttlCharge}") #redirecciono para no producir doble cargo (del post)
+
+
+def checkoutHtml(request, qty, ttl, ttlGrl):
+    context = {
+        "quantity":qty,
+        "totalGrl":ttlGrl,
+        "priceLastPurchase":ttl
+    }
+    return render(request, "store/checkout.html", context)
